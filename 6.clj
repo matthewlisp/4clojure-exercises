ccf056fbe9186adc4e1617b6a4e160bb53e17214
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:27:24 -0300

    Create 6.clj

---
+++ 6.clj
@@ -0,0 +1,11 @@
+;; Title: Intro to Vectors
+;; Link: http://www.4clojure.com/problem/6
+;; Description: Vectors can be constructed several ways. You can compare them with lists. Note: the brackets [] surrounding the blanks __ are part of the test case.
+
+;; QUESTION/TEST CODE
+
+; (= [__] (list :a :b :c) (vec '(:a :b :c)) (vector :a :b :c))
+
+;; SOLUTION CODE
+
+:a :b :c
