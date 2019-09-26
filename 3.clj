1bfeaea0fc3d7ac7eb1220d5a4addd65519c9f93
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:13:24 -0300

    Create 3.clj

---
+++ 3.clj
@@ -0,0 +1,11 @@
+;; Title: Intro to Strings
+;; Link: http://www.4clojure.com/problem/3
+;; Clojure strings are Java strings. This means that you can use any of the Java string methods on Clojure strings.
+
+;; QUESTION/TEST CODE
+
+; (= __ (.toUpperCase "hello world"))
+
+;; SOLUTION CODE
+
+"HELLO WORLD"
