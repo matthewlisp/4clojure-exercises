77feeafaaf0e3490bd4cdac05fc448f29d3fb84f
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:32:25 -0300

    Create 8.clj

---
+++ 8.clj
@@ -0,0 +1,12 @@
+;; Title: Intro to Sets
+;; Link: http://www.4clojure.com/problem/8
+;; Description: Sets are collections of unique values.
+
+;; QUESTION/TEST CODE
+
+; (= __ (set '(:a :a :b :c :c :c :c :d :d)))
+; (= __ (clojure.set/union #{:a :b :c} #{:b :c :d}))
+
+;; SOLUTION CODE
+
+#{:a :b :c :d}
