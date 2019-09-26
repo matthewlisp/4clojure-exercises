49d6e0bdb5053336c626606491ffdbb7ad4e2964
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:30:46 -0300

    Create 7.clj

---
+++ 7.clj
@@ -0,0 +1,12 @@
+;; Title: Vectors: conj
+;; Link: http://www.4clojure.com/problem/7
+;; Description: When operating on a Vector, the conj function will return a new vector with one or more items "added" to the end.
+
+;; QUESTION/TEST CODE
+
+; (= __ (conj [1 2 3] 4))
+; (= __ (conj [1 2] 3 4))
+
+;; SOLUTION CODE
+
+[1 2 3 4]
