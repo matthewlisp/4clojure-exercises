680d0b3a3156ebc436eb403ac71a5edf6b5b7d68
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:21:42 -0300

    Create 4.clj

---
+++ 4.clj
@@ -0,0 +1,11 @@
+;; Title: Intro to Lists
+;; Link: http://www.4clojure.com/problem/4
+;; Description: Lists can be constructed with either a function or a quoted form.
+
+;; QUESTION/TEST CODE
+
+; (= (list __) '(:a :b :c))
+
+;; SOLUTION CODE
+
+:a :b :c
