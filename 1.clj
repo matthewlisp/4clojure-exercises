d2b13c45f72c5ddc264187491ddc05022950bbd6
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:08:59 -0300

    Create 1.clj

---
+++ 1.clj
@@ -0,0 +1,7 @@
+;; Title: Nothing but the Truth
+;; Link: http://www.4clojure.com/problem/1
+;; Description:This is a clojure form. Enter a value which will make the form evaluate to true. Don't over think it! If you are confused, see the getting started page. Hint: true is equal to true.
+
+;; SOLUTION CODE
+
+true
