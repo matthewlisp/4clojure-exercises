d3874cee91b8d5b35ce89cc39ec91e7919d079f8
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:37:18 -0300

    Create 10.clj

---
+++ 10.clj
@@ -0,0 +1,12 @@
+;; Title: Intro to Maps
+;; Link: http://www.4clojure.com/problem/10
+;; Description: Maps store key-value pairs. Both maps and keywords can be used as lookup functions. Commas can be used to make maps more readable, but they are not required.
+
+;; QUESTION/TEST CODE
+
+; (= __ (:b {:a 10, :b 20, :c 30}))
+; (= __ ((hash-map :a 10, :b 20, :c 30) :b))
+
+;; SOLUTION CODE
+
+20
