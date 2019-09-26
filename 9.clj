02883e69002f75fbb394b8c6dc2144117061f2a7
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:33:43 -0300

    Create 9.clj

---
+++ 9.clj
@@ -0,0 +1,11 @@
+;; Title: Sets: conj
+;; Link: http://www.4clojure.com/problem/9
+;; Description: When operating on a set, the conj function returns a new set with one or more keys "added".
+
+;; QUESTION/TEST CODE
+
+; (= #{1 2 3 4} (conj #{1 4 3} __))
+
+;; SOLUTION CODE
+
+2
