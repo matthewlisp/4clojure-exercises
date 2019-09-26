360d6ffbdd047f590943738f08cacf251fa02525
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:39:40 -0300

    Create 11.clj

---
+++ 11.clj
@@ -0,0 +1,11 @@
+;; Title: Maps: Conj
+;; Link: http://www.4clojure.com/problem/11
+;; Description: When operating on a map, the conj function returns a new map with one or more key-value pairs "added".
+
+;; QUESTION/TEST CODE
+
+; (= {:a 1, :b 2, :c 3} (conj {:a 1} __ [:c 3]))
+
+;; SOLUTION CODE
+
+{:b 2}
