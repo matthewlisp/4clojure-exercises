35009954be6f6b9af970b02b0c21f61a06aa3602
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:25:16 -0300

    Create 5.clj

---
+++ 5.clj
@@ -0,0 +1,12 @@
+;; Title: Lists: conj
+;; Link: http://www.4clojure.com/problem/5
+;; Description: When operating on a list, the conj function will return a new list with one or more items "added" to the front. Note that there are two test cases, but you are expected to supply only one answer, which will cause all the tests to pass.
+
+;; QUESTION/TEST CODE
+
+; (= __ (conj '(2 3 4) 1))
+; (= __ (conj '(3 4) 2 1))
+
+;; SOLUTION CODE
+
+(list 1 2 3 4)
