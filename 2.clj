825334bf3b57b988991b6cf28ffa3278f23de8e3
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-26 13:12:00 -0300

    Create 2.clj

---
+++ 2.clj
@@ -0,0 +1,11 @@
+;; Title: Simple Math
+;; Link: http://www.4clojure.com/problem/2
+;; Description: If you are not familiar with polish notation, simple arithmetic might seem confusing. Note: Enter only enough to fill in the blank (in this case, a single number) - do not retype the whole problem.
+
+;; QUESTION/TEST CODE
+
+; (= (- 10 (* 2 3)) __)
+
+;; SOLUTION CODE
+
+4
