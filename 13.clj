f8b23a96911baee7f25ca43cf1e1603c0a8943be
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-27 13:47:43 -0300

    Create 13.clj

---
+++ 13.clj
@@ -0,0 +1,11 @@
+;; Title: Sequences: rest
+;; Link: http://www.4clojure.com/problem/13
+;; Description:  The rest function will return all the items of a sequence except the first.
+
+;; QUESTION/TEST CODE
+
+; (= __ (rest [10 20 30 40]))
+
+;; SOLUTION CODE
+
+[20 30 40]
