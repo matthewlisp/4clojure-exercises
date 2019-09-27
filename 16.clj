598fbcbe3b588ce4b85440b632d858de21f8a62d
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-27 14:07:31 -0300

    Create 16.clj

---
+++ 16.clj
@@ -0,0 +1,14 @@
+;; Title: Hello World
+;; Link: http://www.4clojure.com/problem/16
+;; Description: Write a function which returns a personalized greeting.
+
+;; QUESTION/TEST CODE
+
+;; (= (__ "Dave") "Hello, Dave!")
+;; (= (__ "Jenn") "Hello, Jenn!")
+;; (= (__ "Rhea") "Hello, Rhea!")
+
+
+;; SOLUTION CODE
+
+#(format "Hello, %s!" %)
