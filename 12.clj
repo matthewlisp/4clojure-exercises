35b84a375bf7190648fe1dca4947b11ac5305de4
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-27 13:46:27 -0300

    Create 12.clj

---
+++ 12.clj
@@ -0,0 +1,17 @@
+;; Title: Intro to sequences
+;; Link: http://www.4clojure.com/problem/12
+;; Description:  All Clojure collections support sequencing. You can operate on sequences with functions like first, second, and last.
+
+;; QUESTION/TEST CODE
+
+; (= __ (first '(3 2 1)))
+; (= __ (second [2 3 4]))
+; (= __ (last (list 1 2 3)))
+
+
+
+;; SOLUTION CODE
+
+3
+
+
