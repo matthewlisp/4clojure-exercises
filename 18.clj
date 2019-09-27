18abb0ac84f55566300bd245fe8ae9089665b66e
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-27 14:08:07 -0300

    Create 18.clj

---
+++ 18.clj
@@ -0,0 +1,11 @@
+;; Title: Sequences: filter
+;; Link: http://www.4clojure.com/problem/18
+;; Description: The filter function takes two arguments: a predicate function (f) and a sequence (s). Filter returns a new sequence consisting of all the items of s for which (f item) returns true.
+
+;; QUESTION/TEST CODE
+
+;; (= __ (filter #(> % 5) '(3 4 5 6 7)))
+
+;; SOLUTION CODE
+
+'(6 7)
