ba7ac10c1f1761f52189e76d8f3639bb9bf252d3
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-27 13:50:12 -0300

    Create 14.clj

---
+++ 14.clj
@@ -0,0 +1,14 @@
+;; Title: Intro to functions
+;; Link: http://www.4clojure.com/problem/14
+;; Description: Clojure has many different ways to create functions.
+
+;; QUESTION/TEST CODE
+
+;(= __ ((fn add-five [x] (+ x 5)) 3))
+;(= __ ((fn [x] (+ x 5)) 3))
+;(= __ (#(+ % 5) 3))
+;(= __ ((partial + 5) 3))
+
+;; SOLUTION CODE
+
+8
