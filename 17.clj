16869ee6203f8e7c9ef6f6a3da5220db9324a044
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-27 14:07:50 -0300

    Create 17.clj

---
+++ 17.clj
@@ -0,0 +1,11 @@
+;; Title: Sequences: map
+;; Link: http://www.4clojure.com/problem/17
+;; Description: The map function takes two arguments: a function (f) and a sequence (s). Map returns a new sequence consisting of the result of applying f to each item of s. Do not confuse the map function with the map data structure.
+
+;; QUESTION/TEST CODE
+
+;; (= __ (map #(+ % 5) '(1 2 3)))
+
+;; SOLUTION CODE
+
+'(6 7 8)
