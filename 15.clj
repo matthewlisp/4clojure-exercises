784cee0600e44c0a252cd5d8bfac9a5df5eb9a93
matthewlisp <54231022+matthewlisp@users.noreply.github.com>
2019-09-27 14:07:16 -0300

    Create 15.clj

---
+++ 15.clj
@@ -0,0 +1,14 @@
+;; Title: Double Down
+;; Link: http://www.4clojure.com/problem/15
+;; Description: Intro to functions
+
+;; QUESTION/TEST CODE
+
+;; (= (__ 2) 4)
+;; (= (__ 3) 6)
+;; (= (__ 11) 22)
+;; (= (__ 7) 14)
+
+;; SOLUTION CODE
+
+#(* % 2)
